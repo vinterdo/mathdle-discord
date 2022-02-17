820fbcb2c2afbe4c668ccbcf9a7db0a6f6d769aa
Vinterdo <pio4426@gmail.com>
2022-02-17 23:20:26 +0100

    Fix gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const discord_js_1 = require("discord.js");
-const config_json_1 = require("./config.json");
-const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
-client.once('ready', () => {
-    console.log('Ready!');
-});
-client.login(config_json_1.token);
